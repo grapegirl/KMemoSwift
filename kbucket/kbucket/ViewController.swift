4bc407a1ebcac5fab39955a921b877978eacbb2d
grapegirl <kikiplus2030@gmail.com>
2017-06-18 16:53:27 +0900

    Main stroybard
    화면 추가

--- kbucket/kbucket/ViewController.swift
+++ kbucket/kbucket/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +34,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
+    
+    @IBAction func btSetting(sender : UIButton){
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
